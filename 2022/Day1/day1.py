5157852b71c7a2b639bfc9ab25f08ddf8abfdaef
Charles Héon <charles.heon@nventive.com>
2022-12-09 11:37:46 -0500

    day1 finished

--- 2022/Day1/day1.py
+++ 2022/Day1/day1.py
@@ -1,4 +1,6 @@
-elvesCalories = """1000
+from input import data
+
+testData = """1000
 2000
 3000
 
@@ -13,8 +15,21 @@ elvesCalories = """1000
 
 10000"""
 
-def parse(arr):
-    arr = arr.splitlines()
-    print(arr)
+parsedArray = []
 
-parse(elvesCalories)
+def getSummedTop3(array):
+    return sum(sorted(array, reverse=True)[0:3])
+
+def parseData(elvesCalories):
+    elvesCalories = elvesCalories.split("\n\n")    
+
+    for calories in elvesCalories:
+        calories = sum(list(map(int, calories.split("\n"))))
+        parsedArray.append(calories)
+
+    top1 = str(max(parsedArray))
+    top3 = str(getSummedTop3(parsedArray))
+
+    return "Top 1: " + top1 + "\n" + "Total of top 3: " + top3
+
+print(parseData(data))
