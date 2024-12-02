881bbcb5764f137c3acb6effc3a95302e58bec32
Charles Heon <cheon@axso.io>
2024-12-02 10:59:38 -0500

    move some comments

--- 2024/01/main.ts
+++ 2024/01/main.ts
@@ -10,20 +10,20 @@ const testData = `3   4
 const getDistance = (data: string) => {
   let total = 0;
 
-  // 1. parse each line and split by double space
+  // 1. parse each line and split by white space
   const input = data.split("\n");
   const splitValues = input.map((val) => val.split(/\s+/));
 
   // 2. sort both array ascending
-  const firstArray = splitValues
+  const firstArray = splitValues // Could be replace by a array.reduce
     .map((val) => Number(val[0]))
     .sort((a, b) => a - b);
   const secondArray = splitValues
     .map((val) => Number(val[1]))
     .sort((a, b) => a - b);
 
+  // 3. get difference between 2 numbers
   firstArray.forEach((num, index) => {
-    // 3. get difference between 2 numbers
     const comparedNum = secondArray[index];
 
     // 4. add differences together
