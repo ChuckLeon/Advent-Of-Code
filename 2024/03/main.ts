93bd3b991e4c40189dcd73de08208608898104c9
Charles Heon <cheon@axso.io>
2024-12-04 11:58:08 -0500

    day3-part1

---
+++ 2024/03/main.ts
@@ -0,0 +1,23 @@
+const filePath = "./data.txt";
+
+const data = await Deno.readTextFile(filePath);
+const testData = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+const testData_part2 =
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+const parseData_1 = (data: string) => {
+  let total = 0;
+  let match;
+  const regex = /mul\((\d+),(\d+)\)/g;
+
+  while ((match = regex.exec(data)) !== null) {
+    const firstNum = Number(match[1]);
+    const secondNum = Number(match[2]);
+
+    total += firstNum * secondNum;
+  }
+
+  console.log(total);
+};
+
+parseData_1(data);
