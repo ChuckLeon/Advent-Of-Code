b26398f3938ac26cdd26afebee465f29fd50c3bd
Charles Heon <cheon@axso.io>
2024-12-04 13:00:08 -0500

    day3-part2

--- 2024/03/main.ts
+++ 2024/03/main.ts
@@ -20,4 +20,29 @@ const parseData_1 = (data: string) => {
   console.log(total);
 };
 
-parseData_1(data);
+const parseData_2 = (data: string) => {
+  let total = 0;
+  const regex = /mul\((\d+),(\d+)\)/g;
+
+  let match;
+
+  while ((match = regex.exec(data)) !== null) {
+    const mulStartIndex = match.index; // The position where mul() starts
+
+    if (
+      data.lastIndexOf("do", mulStartIndex) >
+        data.lastIndexOf("don't", mulStartIndex) ||
+      (data.lastIndexOf("do", mulStartIndex) === -1 &&
+        data.lastIndexOf("don't", mulStartIndex) === -1)
+    ) {
+      const firstNum = Number(match[1]);
+      const secondNum = Number(match[2]);
+
+      total += firstNum * secondNum;
+    }
+  }
+
+  console.log(total);
+};
+
+parseData_2(data);
