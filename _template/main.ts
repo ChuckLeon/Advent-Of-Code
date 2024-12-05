e2599ec1ef200696d005a92d12411f5313835be7
Charles Heon <cheon@axso.io>
2024-12-05 08:25:08 -0500

    add template

---
+++ _template/main.ts
@@ -0,0 +1,11 @@
+import { data } from "./data.ts";
+
+const testData = ``;
+
+const dataToArray = (data: string) => {
+  return data.split("\n").map((val) => val.split(""));
+};
+
+const part1 = (data: string) => {};
+
+const part2 = (data: string) => {};
